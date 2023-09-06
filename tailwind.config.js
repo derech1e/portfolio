fb0b11a3fdab5f39187bfd9008df35b219ff2daf
derech1e <nurkthomas@gmail.com>
2023-09-06 22:35:05 +0200

    add imprint

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,17 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}', './NavBar.jsx'],
 	theme: {
+		fontFamily: {
+			sans: ["Source Sans Pro", ...defaultTheme.fontFamily.sans],
+		},
 		extend: {
 			borderWidth: {
 				'6': '6px',
 				'0.5': '0.5px',
-			}
+			},
 		},
 	},
 	plugins: [],
