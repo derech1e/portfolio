33d78bfdbb9d766fec1375cefbbeafbc3d24bc5a
derech1e <nurkthomas@gmail.com>
2023-09-07 11:47:11 +0200

    adjust design

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,17 +2,23 @@
 const defaultTheme = require("tailwindcss/defaultTheme");
 
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}', './NavBar.jsx'],
-	theme: {
-		fontFamily: {
-			sans: ["Source Sans Pro", ...defaultTheme.fontFamily.sans],
-		},
-		extend: {
-			borderWidth: {
-				'6': '6px',
-				'0.5': '0.5px',
-			},
-		},
-	},
-	plugins: [],
+    content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}', './NavBar.jsx'],
+    theme: {
+        fontFamily: {
+            sans: ["Source Sans Pro", ...defaultTheme.fontFamily.sans],
+        },
+        extend: {
+            borderWidth: {
+                '6': '6px',
+                '0.5': '0.5px',
+            },
+            fontSize: {
+                '2_5xl': ['1.625rem', {
+					lineHeight: '1.5',
+				}
+                ]
+            }
+        },
+    },
+    plugins: [],
 }
